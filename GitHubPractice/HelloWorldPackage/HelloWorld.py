1d175a226b130f26ecb918f978e65de6678fda8c
bluej <bluej@DESKTOP-2NJ279P>
2022-10-18 08:50:27 -0400

    Added print

--- GitHubPractice/HelloWorldPackage/HelloWorld.py
+++ GitHubPractice/HelloWorldPackage/HelloWorld.py
@@ -2,7 +2,13 @@
 Created on Oct 18, 2022
 
 @author: bluej
+
+Right-click project, then Team, then Share Project
+
+Window > Show View > Other... > Git > Git Staging
+
 '''
 # GitHub Practice code
 
 print("Hello World")
+print("Football Bearcats")
